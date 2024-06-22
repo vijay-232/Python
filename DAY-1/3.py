1232c78a770dbd34ec7fee014b1931c74e8ad73f
vijay-232 <vijaysrisurya23@gmail.com>
2024-06-22 08:09:28 +0530

    Changed 3rd question code

--- DAY-1/3.py
+++ DAY-1/3.py
@@ -1,14 +1,19 @@
 s = input()
 k = int(input())
 ans = []
-if len(s) % k == 0:
-    for i in range(k):
-        ans.append(s[i * k:(i + 1) * k])
-elif len(s) % k != 0:
-    for i in range(len(s) % k):
-        ans.append(s[i * k:(i + 1) * k])
-    ans.append(s[(len(s) % k) * k:])
+num_parts = len(s) / k
+value = len(s) % k
 
+for i in range(int(num_parts)):
+    if len(s) % k == 0:
+        temp = s[i * k:(i + 1) * k]
+    else:
+        temp = s[i * k:(i + 1) * k]
+    ans.append(temp)
+
+if len(s) % k != 0:
+    ans.append(s[int(num_parts) * k:])
+print(ans)
 for word in ans:
     temp = ''
     for letter in word:
